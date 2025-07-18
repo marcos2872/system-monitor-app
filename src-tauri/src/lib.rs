642be4cfd8dab05d7e032a53913b4fa2137f5be2
Marcos Brito <marcos.brito@sp.senai.br>
2025-07-18 15:24:10 -0300

    .

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,6 +11,35 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            println!("Setting up system tray...");
+            
+            // Configure system tray
+            if let Some(tray) = app.tray_by_id("main") {
+                println!("System tray found with ID: main");
+                tray.on_tray_icon_event(|tray, event| {
+                    println!("Tray icon event: {:?}", event);
+                    match event {
+                        tauri::tray::TrayIconEvent::Click { .. } => {
+                            let app = tray.app_handle();
+                            if let Some(window) = app.get_webview_window("main") {
+                                if window.is_visible().unwrap_or(false) {
+                                    let _ = window.hide();
+                                } else {
+                                    let _ = window.show();
+                                    let _ = window.set_focus();
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                });
+            } else {
+                println!("System tray not found!");
+            }
+            
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
