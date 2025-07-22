da60349d472e34bd028ade8705b54b88b99efd22
Marcos Brito <marcos.brito@sp.senai.br>
2025-07-22 15:56:16 -0300

    .tt

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,78 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::{
+    menu::{Menu, MenuBuilder, MenuItem},
+    tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
+    Manager, Runtime,
+};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn show_main_window(window: tauri::Window) {
+    window.get_webview_window("main").unwrap().show().unwrap();
+}
+
+#[tauri::command]
+fn hide_main_window(window: tauri::Window) {
+    window.get_webview_window("main").unwrap().hide().unwrap();
+}
+
+fn create_tray_menu<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::Result<Menu<R>> {
+    let show_item = MenuItem::with_id(app, "show", "Show", true, None::<&str>)?;
+    let hide_item = MenuItem::with_id(app, "hide", "Hide", true, None::<&str>)?;
+    let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+    
+    let menu = MenuBuilder::new(app)
+        .items(&[&show_item, &hide_item, &quit_item])
+        .build()?;
+    
+    Ok(menu)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, show_main_window, hide_main_window])
+        .setup(|app| {
+            let menu = create_tray_menu(&app.handle())?;
+            
+            let _tray = TrayIconBuilder::with_id("main-tray")
+                .menu(&menu)
+                .on_menu_event(move |app, event| match event.id.as_ref() {
+                    "show" => {
+                        let window = app.get_webview_window("main").unwrap();
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                    "hide" => {
+                        app.get_webview_window("main").unwrap().hide().unwrap();
+                    }
+                    "quit" => {
+                        app.exit(0);
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: tauri::tray::MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        let app = tray.app_handle();
+                        if let Some(webview_window) = app.get_webview_window("main") {
+                            let _ = webview_window.show();
+                            let _ = webview_window.set_focus();
+                        }
+                    }
+                })
+                .build(app)?;
+            
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
